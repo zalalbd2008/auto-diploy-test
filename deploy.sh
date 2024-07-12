abc996fc2b4b3d9dcf7cbdbec7e4e31f43a12884
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-07-13 00:35:34 +0600

    test2

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,10 @@
 cd /root/auto-diploy-test/
 
 # Pull the latest code from GitHub
-git pull origin main
+git pull 
 
 # Install dependencies
-npm install
-
-# Build the project (for Next.js)
-npm run build
+# npm install
 
 # Restart the application with PM2
 pm2 restart server
